6d9723f77e55d9f218271db20ff8cd28c633d67c
HuynhThanhQuan <hthquan28@gmail.com>
2023-10-23 13:15:06 +0700

    refactor code to export all table with report date

--- gw9028/feature/sql/features/generate_features.py
+++ gw9028/feature/sql/features/generate_features.py
@@ -1,12 +1,4 @@
 import os
-import logging
-
-# Set up logging
-logging.basicConfig(filename='generate_tables.log', level=logging.DEBUG)
-
-logging.info('Starting script...')
-
-
 
 # Assuming RPT_DT is a string in the format "YYYY-MM-DD"
 RPT_DT = "2023-10-01"
@@ -17,8 +9,6 @@ folder_path = "./"
 
 REPORT_DATE_FOLDER = os.path.join(folder_path, 'report_date', RPT_DT)
 PLACEHOLDER_FOLDER = os.path.join(folder_path, 'placeholder')
-logging.info('REPORT_DATE_FOLDER: ' + REPORT_DATE_FOLDER)
-logging.info('PLACEHOLDER_FOLDER: ' + PLACEHOLDER_FOLDER)
 
 if not os.path.exists(REPORT_DATE_FOLDER):
     os.makedirs(REPORT_DATE_FOLDER) 
@@ -33,9 +23,5 @@ for filename in os.listdir(PLACEHOLDER_FOLDER):
         file_content = file_content.replace("{RPT_DT}", RPT_DT)
         file_content = file_content.replace("{RPT_DT_TBL}", RPT_DT_TBL)
         # Write the updated content back to the file
-        filename_only = os.path.splitext(os.path.basename(filename))[0]
-        filename_dt = filename_only + '_' + RPT_DT_TBL + '.sql'
-        with open(os.path.join(REPORT_DATE_FOLDER, filename_dt), "w") as file:
+        with open(os.path.join(REPORT_DATE_FOLDER, filename), "w") as file:
             file.write(file_content)
-            logging.info('Created file: ' + filename_dt)
-logging.info('Script completed.')
