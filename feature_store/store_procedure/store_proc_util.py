231bef97c566f8bbd45065a5cf5418258bcc8aa5
os-quanht190494 <os-quanht190494@sacombank.com>
2023-12-18 14:30:19 +0700

    done proc template

--- feature_store/store_procedure/store_proc_util.py
+++ feature_store/store_procedure/store_proc_util.py
@@ -1,8 +1,9 @@
 import sys
-sys.path.append('../sql/')
+sys.path.append('..')
 
 import os
 import yaml
+import util_func
 
 
 config_proc_fp = 'config/proc.yaml'
@@ -29,7 +30,7 @@ def fill_in_template(config_data, template_content):
     tbl_nm = casa_section['TABLE_NAME']
     features = casa_section['FEATURE']
 
-    content = []
+    agg_content = []
     # Loop through feature file
     # Each feature replace:
     #   {TBL_NM} -> tbl_nm var
@@ -39,13 +40,26 @@ def fill_in_template(config_data, template_content):
         feat_fp = os.path.join(feat_template_fd, feat + '.sql')
         with open(feat_fp, 'r') as f:
             content = ''.join(f.readlines())
+            _, _, sql_sec = util_func.extract_desc_yaml_section_from_string(content)
+            sql_sec = sql_sec.replace('{TBL_NM}', tbl_nm)
+            sql_sec = sql_sec.replace('CINS_TMP_CUSTOMER_{RPT_DT_TBL}',src_tbl)
+            sql_sec = sql_sec.replace("'{RPT_DT}'", 'RPT_DT')
+            agg_content.append(sql_sec)
+    agg_content = util_func.COMMIT_CHECKPOINT.join(agg_content).strip()
 
+    # Read proc template
+    # Replace ${FEATURE_SCRIPTS}$ -> agg_content
+    proc_content = template_content.replace('${FEATURE_SCRIPTS}$', agg_content)
+
+    # Write into file and store at script
+    with open('script/proc.sql','w') as f:
+        f.writelines(proc_content)
+    print('Done - stored at script/proc.sql')
 
 def run():
     config_data = read_yaml_config()
     template_content = read_template()
     fill_in_template(config_data, template_content)
-
 
 
 if __name__ == '__main__':
